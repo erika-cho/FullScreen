0d92627a54137418eaafa7e2bca6e09b8dd28fb1
Erika Cho <cho.erika@lib-gate.co.jp>
2014-11-01 04:32:53 +0900

    window.onresizeでサイズを調整するように変更

--- jquery.fullScreen.js
+++ jquery.fullScreen.js
@@ -11,22 +11,6 @@
       groundUrl = groundValue.match(/^url\(["']?(.*?)["']?\)/i)[1];
       groundBox.style.cssText = 'position: relative;' + 'width: 100%;' + 'height: 100%;' + 'overflow: hidden;';
 
-      var setBackground = function(imgW, imgH) {
-        var winW = document.documentElement.clientWidth;
-        var winH = document.documentElement.clientHeight;
-        var scaleW = winW / imgW;
-        var scaleH = winH / imgH;
-        var fixScale = Math.max(scaleW, scaleH);
-        var setW = imgW * fixScale;
-        var setH = imgH * fixScale;
-        var moveX = Math.floor((winW - setW) / 2);
-        var moveY = Math.floor((winH - setH) / 2);
-
-        groundBox.innerHTML = '<img src="' + groundUrl + '" alt="" width="' + setW + '" height="' + setH + '" style="position: absolute; top: ' + moveX + '; left: ' + moveY + 'px;" />';
-
-        element.appendChild(groundBox);
-      };
-
       var img = new Image();
       img.src = "";
 
@@ -34,6 +18,30 @@
         setBackground(img.width, img.height);
       }
       img.src = groundUrl;
+
+      var winW = document.documentElement.clientWidth;
+      var winH = document.documentElement.clientHeight;
+
+      var setBackground = function(imgW, imgH) {
+        var scaleW = winW / imgW;
+        var scaleH = winH / imgH;
+        var fixScale = Math.max(scaleW, scaleH);
+        var setW = imgW * fixScale;
+        var setH = imgH * fixScale;
+        var moveX = Math.floor((winW - setW) / 2);
+        var moveY = Math.floor((winH - setH) / 2);
+        groundBox.innerHTML = '<img src="' + groundUrl + '" alt="" width="' + setW + '" height="' + setH + '" />';
+        element.appendChild(groundBox);
+      };
+
+      var resizeWindow = function() {
+        winW = document.documentElement.clientWidth;
+        winH = document.documentElement.clientHeight;
+
+        setBackground(img.width, img.height);
+      };
+
+      window.onresize = resizeWindow;
 
     });
   };
